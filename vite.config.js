2153871948661a7b648746df1af8b8cf70a82e41
d_nesh <dine11649@gmail.com>
2024-09-22 21:09:38 +0900

    Configure vite for npm run dev and update alpine

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,21 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // This allows access from outside the Vagrant box
+        port: 5173,
+        hmr: {
+            host: 'reverb.test', // Your local development domain
+            // host: '0701-217-178-54-242.ngrok-free.app', // Your local development domain(this is for while runnig ngrok)
+            protocol: 'ws', // Ensure WebSocket is used for HMR
+            // protocol: 'wss', // Ensure WebSocket is used for HMR(this is for while runnig ngrok)
+            port: 5173,
+        },
+        watch: {
+            usePolling: true, // Useful in virtualized environments
+        },
+        open: false, //open browser not supported in my envioronment
+        strictPort: true,
+        https: false,
+    }
 });
